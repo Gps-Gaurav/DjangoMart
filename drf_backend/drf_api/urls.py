f2b1227615c55f48a067a31bd3f8137ee0ce0337
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-06-05 23:48:32 +0530

    commit

--- drf_backend/drf_api/urls.py
+++ drf_backend/drf_api/urls.py
@@ -6,9 +6,9 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('', views.getRoutes,name="getRoutes"),
-    # path('products/',views.getProducts,name="getProducts"),
-    # path('product/<str:pk>',views.getProduct,name="getProduct"),
-    # path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('products/',views.getProducts,name="getProducts"),
+    path('product/<str:pk>',views.getProduct,name="getProduct"),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('users/profile/',views.getUserProfiles,name="getUserProfiles"),
     # path('users/',views.getUsers,name="getUsers"),
     # path('users/register/',views.registerUser,name="register"),
--- drf_backend/drf_api/views.py
+++ drf_backend/drf_api/views.py
@@ -1,8 +1,54 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.decorators import api_view,permission_classes
+# from .products import products
+from .models import Products
+from .serializer import ProductsSerializer,UserSerializer,UserSerializerWithToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
+# for sending mails and generate token
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode,urlsafe_base64_encode
+# from .utils import TokenGenerator,generate_token
+from django.utils.encoding import force_bytes,force_text,DjangoUnicodeDecodeError
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.views.generic import View
 
 # Create your views here.
 @api_view(['GET'])
 def getRoutes(request):
     return Response('Hello Anees')
+
+@api_view(['GET'])
+def getProducts(request):
+    products=Products.objects.all()
+    serializer=ProductsSerializer(products,many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProduct(request,pk):
+    product=Products.objects.get(_id=pk)
+    serializer=ProductsSerializer(product,many=False)
+    return Response(serializer.data)
+
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        serializer=UserSerializerWithToken(self.user).data
+        for k,v in serializer.items():
+            data[k]=v       
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class=MyTokenObtainPairSerializer
+
