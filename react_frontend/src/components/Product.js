de77e11b176f89256e065a193fb67f80ed8cdf5b
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-06-08 21:29:22 +0530

    commit_r

--- react_frontend/src/components/Product.js
+++ react_frontend/src/components/Product.js
@@ -1,38 +1,52 @@
-import React from 'react'
-import {Card} from "react-bootstrap"
-import Rating from '../components/Rating'
-import {Link} from 'react-router-dom'
-function Product({product}) {
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import { Link } from "react-router-dom";
+
+function Product({ product }) {
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card className="my-3 p-2 rounded shadow-sm border-0 product-card" style={{ minHeight: "420px" }}>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img
+          src={product.image}
+          variant="top"
+          className="img-fluid rounded product-img"
+        />
+      </Link>
 
-        <Link to={`/product/${product._id}`}>
-            <Card.Img src={product.image} />
-        </Link>
-        <Card.Body>
-        <Link to={`/product/${product._id}`} className='text-dark'>
-            <Card.Title as="h3">
-                {product.productname}
+      <Card.Body className="d-flex flex-column justify-content-between">
+        <div>
+          <Link to={`/product/${product._id}`} className="text-decoration-none text-dark">
+            <Card.Title as="h5" className="fw-semibold mb-2">
+              {product.productname}
             </Card.Title>
-           </Link>
+          </Link>
 
-           <Card.Text as="div">
-           <div className="my-3">
-                {product.rating}  from {product.numReviews}    reviews
-            </div>
-           </Card.Text>
-            
-        <Card.Text as="h6">
-            {product.price} Rs
-        </Card.Text>
-        <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-                color={"#f8e825"}
-              />
-        </Card.Body>
+          <div className="mb-2 text-muted small">
+            <strong>{product.rating}</strong> from {product.numReviews} reviews
+          </div>
+
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+            color="#f8e825"
+          />
+
+          <Card.Text as="h6" className="mt-2 text-success fw-bold">
+            ₹{product.price}
+          </Card.Text>
+        </div>
+
+        <div className="mt-3">
+          <Link to={`/product/${product._id}`}>
+            <Button variant="outline-primary" className="w-100">
+              More Details
+            </Button>
+          </Link>
+        </div>
+      </Card.Body>
     </Card>
-  )
+  );
 }
 
-export default Product
+export default Product;
--- react_frontend/src/components/screens/HomeScreen.js
+++ react_frontend/src/components/screens/HomeScreen.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import Product from "../Product";
+import { Container, Row, Col, Alert } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
 import { listProducts } from "../../actions/productsActions";
-import { useDispatch, useSelector } from 'react-redux';
+import Product from "../Product";
 import Loader from "../Loader";
 import Message from "../Message";
 
@@ -16,19 +16,20 @@ function HomeScreen() {
   }, [dispatch]);
 
   return (
-    <Container>
-      <br />
-      <h1>Products</h1>
-      <marquee behavior="" direction="">
-        Welcome to DjangoMart Shopping! Get the best deals on all products!
-      </marquee>
+    <Container className="py-4">
+      <div className="text-center mb-4">
+        <h2 className="fw-bold">Shop Top Deals on DjangoMart</h2>
+        <Alert variant="info" className="mt-3 mb-0 rounded-pill shadow-sm">
+          🎉 Welcome to DjangoMart! Enjoy exclusive offers on your favorite products!
+        </Alert>
+      </div>
 
       {loading ? (
         <Loader />
       ) : error ? (
         <Message variant="danger">{error}</Message>
       ) : (
-        <Row>
+        <Row className="g-4">
           {products && products.length > 0 ? (
             products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
@@ -36,7 +37,7 @@ function HomeScreen() {
               </Col>
             ))
           ) : (
-            <Col>
+            <Col className="text-center">
               <Message variant="info">No products found.</Message>
             </Col>
           )}
