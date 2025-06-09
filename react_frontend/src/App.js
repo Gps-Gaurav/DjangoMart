0e9486419ce26a0f2b2030efcb075e25f7379a97
Gaurav Pratap Singh <gauravpratapsingh321@gmail.com>
2025-06-09 22:02:44 +0530

    commit

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
@@ -8,24 +9,36 @@ import LoginScreen from "./screens/LoginScreen";
 import SignupScreen from "./screens/SignupScreen";
 import CartScreen from "./screens/CartScreen";
 import NotFoundScreen from "./screens/NotFoundScreen";
+import WaitingScreen from './screens/WaitingScreen';
 
 function App() {
-  return (
-    <div className="d-flex flex-column min-vh-100">
-      <Header />
-      <main className="py-3 flex-grow-1">
-        <Routes>
-          <Route path="/" element={<HomeScreen />} exact />
-          <Route path="/product/:id" element={<ProductScreen />} />
-          <Route path="/login" element={<LoginScreen />} />
-          <Route path="/signup" element={<SignupScreen />} />
-          <Route path="/cart" element={<CartScreen />} />
-          <Route path="*" element={<NotFoundScreen />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="d-flex flex-column site-container">
+            <Header />
+            <main className="main-content">
+                <Container>
+                    <Routes>
+                        {/* Home Route */}
+                        <Route path="/" element={<HomeScreen />} />
+                        
+                        {/* Product Routes */}
+                        <Route path="/product/:id" element={<ProductScreen />} />
+                        <Route path="/cart" element={<CartScreen />} />
+                        
+                        {/* Auth Routes */}
+                        <Route path="/login" element={<LoginScreen />} />
+                        <Route path="/signup" element={<SignupScreen />} />
+                        <Route path="/verify-email" element={<WaitingScreen />} />
+                        <Route path="/activate/:uid/:token" element={<WaitingScreen />} />
+                        
+                        {/* 404 Route */}
+                        <Route path="*" element={<NotFoundScreen />} />
+                    </Routes>
+                </Container>
+            </main>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- react_frontend/src/index.js
+++ react_frontend/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store';
+import './bootstrap.min.css';
+import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
